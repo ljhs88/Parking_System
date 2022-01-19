1479338cc9d43db6f6818fa8ca6c72ee4ce49aac
zlb-2002 <1335654255@qq.com>
2022-01-19 22:27:45 +0800

    commit 2022-1-19

--- ParkingSystem/register/src/main/java/com/xiyou3g/select/customer/register/Cus_LoginActivity.java
+++ ParkingSystem/register/src/main/java/com/xiyou3g/select/customer/register/Cus_LoginActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 
 import android.view.WindowManager;
@@ -15,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.xiyou3g.select.customer.register.util.CommonTool;
 import com.xiyou3g.select.customer.register.util.NumberMatch;
 
 import java.util.Objects;
@@ -32,17 +36,14 @@ public class Cus_LoginActivity extends AppCompatActivity implements View.OnClick
 
     }
 
-    private void hint_change() {
+    public void hint_change () {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
             actionBar.hide();
-        Objects.requireNonNull(getSupportActionBar()).hide();
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+
     }
+
 
     @Override
     public void onClick(View view) {
@@ -51,14 +52,26 @@ public class Cus_LoginActivity extends AppCompatActivity implements View.OnClick
                 EditText accountEditText = findViewById(R.id.cus_account);
                 EditText passwordEditText = findViewById(R.id.cus_password);
                 accountMatch(accountEditText.getText().toString());
+                accountEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(11)});
                 break;
             case R.id.new_register:
+                start(R.id.new_register);
                 break;
             case R.id.phone_number_login:
+                start(R.id.phone_number_login);
                 break;
             case R.id.forget_password:
+                start(R.id.forget_password);
                 break;
         }
+    }
+
+    public void start(int id) {
+        Intent intent = new Intent(this, Cus_RegisterActivity.class);
+        TextView textView = findViewById(id);
+        String string = textView.getText().toString();
+        intent.putExtra("text", string);
+        startActivity(intent);
     }
 
     public void accountMatch (String phone_number) {
---
+++ ParkingSystem/register/src/main/java/com/xiyou3g/select/customer/register/util/TimeCountUtil.java
@@ -0,0 +1,44 @@
+package com.xiyou3g.select.customer.register.util;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.Button;
+
+import com.xiyou3g.select.customer.register.R;
+
+public class TimeCountUtil extends CountDownTimer {
+
+    private final Activity activity;
+    private final Button button;
+
+    public TimeCountUtil(long millisInFuture, long countDownInterval, Activity activity, Button button) {
+        super(millisInFuture, countDownInterval);
+        this.activity = activity;
+        this.button = button;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onTick(long l) {
+        button.setClickable(false);
+        button.setText(l/1000 + "秒后可重新发送");
+
+        button.setBackground(activity.getDrawable(R.drawable.get_code_button));
+        Spannable spannable = new SpannableString(button.getText().toString());
+
+        spannable.setSpan(new ForegroundColorSpan(Color.RED), 0, 2, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        button.setText(spannable);
+    }
+
+    @Override
+    public void onFinish() {
+        button.setText("重新获取验证码");
+        button.setClickable(true);
+        button.setBackground(activity.getResources().getDrawable(R.drawable.get_code_button));
+    }
+}
