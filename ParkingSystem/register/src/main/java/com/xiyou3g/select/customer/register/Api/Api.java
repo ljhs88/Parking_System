dd5577f516c330c386844fa3a43d650356314149
zlb-2002 <1335654255@qq.com>
2022-01-22 19:30:26 +0800

    commit 2022-01-22

---
+++ ParkingSystem/register/src/main/java/com/xiyou3g/select/customer/register/Api/Api.java
@@ -0,0 +1,25 @@
+package com.xiyou3g.select.customer.register.Api;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface Api {
+
+    @POST("post")
+    @FormUrlEncoded
+    Call<ResponseBody> post(@Field("useName") String useName, @Field("passWord") String passWord);
+
+    @GET("get")
+    Call<ResponseBody> get(@Query("key") String useName, @Query("passWord") String passWord);
+
+    @POST("postBody")
+    Call<ResponseBody> postBody(@Body RequestBody body);
+
+}
