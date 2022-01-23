06a097c61856ca68297a9522eedb394d317054ae
zlb-2002 <1335654255@qq.com>
2022-01-23 21:17:44 +0800

    commit 2022-01-23

--- ParkingSystem/information/src/main/java/com/xiyou3g/information/Cus_InformationActivity.java
+++ ParkingSystem/information/src/main/java/com/xiyou3g/information/Cus_InformationActivity.java
@@ -3,8 +3,11 @@ package com.xiyou3g.information;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 
 @Route(path = "/information/Cus_InformationActivity")
 public class Cus_InformationActivity extends AppCompatActivity {
@@ -13,5 +16,12 @@ public class Cus_InformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cus_information);
+        TextView textView = findViewById(R.id.text1);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ARouter.getInstance().build("/customer/Cus_LoginActivity").navigation();
+            }
+        });
     }
 }
